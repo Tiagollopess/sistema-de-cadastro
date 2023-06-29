68c453e1d55290f15c7c58273fcfc1b4bcb2ffa3
Naiara <naiara.lindolfo@hotmail.com>
2023-06-29 11:40:50 -0300

    atualizando o erro fatal que eu fiz

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log ("Rodando o Sistema de gerenciamento de jogos")
 console.log ("aplicação esta se conectando ao bando de dados")
-
+//modificando o app js 
+console.log ("reparando o erro fatal")
