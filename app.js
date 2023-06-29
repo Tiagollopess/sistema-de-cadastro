5eaa1e0a32bc24e93ee2ee1fd5b9df8f2309a278
Naiara <naiara.lindolfo@hotmail.com>
2023-06-29 11:22:23 -0300

    atualizando o app.js

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log ("aplicação esta se conectando ao bando de dados")
 //mais um console log de teste:
 console.log("Estamos ficando malucos enquanto aprendemos a comitar")
 console.log("Enviando para o GitHub")
+
