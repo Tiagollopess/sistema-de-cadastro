cc1e2ff9e314f3c699f03c4011804bc82c44f052
Naiara <naiara.lindolfo@hotmail.com>
2023-07-04 10:09:42 -0300

    atualizando arquivo app js e excluindo contato

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log ("Rodando o Sistema de gerenciamento de jogos")
 console.log ("aplicação esta se conectando ao bando de dados")
-//modificando o app js 
-console.log ("reparando o erro fatal")
-console.log ("treinando o uso dos comandos de commit no git")
+
